842edb2274672f8e85dd56dbb72e921268fcc30f
crycetruly <crycetruly@gmail.com>
2019-05-20 07:02:42 +0300

    ft(linkedIn): Add LinkedIn Authentication

--- allauthdjango/apps/authentication/tests/test_base.py
+++ allauthdjango/apps/authentication/tests/test_base.py
@@ -4,6 +4,7 @@
 
 from rest_framework.test import APIClient, APITestCase
 from django.urls import reverse
+from .test_data.test_data import mytestcode
 
 
 class BaseTest(APITestCase):
@@ -18,3 +19,8 @@ class BaseTest(APITestCase):
         response = self.client.get(self.email_url, format="json")
         message = response.data['msg']
         self.assertEqual(message, "Welcome email")
+
+    def test_linked_in_auth(self):
+        response = self.client.get(
+            self.linkedin_url+"?code="+mytestcode, format="json")
+        self.assertEqual(response.status_code, 400)
--- allauthdjango/apps/authentication/tests/test_data/test_data.py
+++ allauthdjango/apps/authentication/tests/test_data/test_data.py
@@ -3,3 +3,5 @@ user = {
     "email": "crycetruly@gmail.com",
     "password": "XZXZXZVVasf"
 }
+mytestcode = """AQQWJGTEldq0yBnmYvOYHXWbSuFP6pg89RVmElfzeApeT3YcdCzq1ZArhTbaUKamvWVdwNDH9sMcOBF3rbwcihY79pEjt7v6opW9xnrH3Ola1ssW1zdFdhwtX3vzW10r734YzZHqAkX3vzN7_cAQiDc9URl3TbnDVMuGKU2_3DZ_mRiYozPrPDkQWM7K2Q
+"""
